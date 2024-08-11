8fdf32f1a86e1f0593d40bc4050ff33366ebda1f
Aizen1d <ddreataza@gmail.com>
2024-08-11 19:53:36 +0800

    Color scheme for elements created

---
+++ constants/colorsScheme.ts
@@ -0,0 +1,11 @@
+export default {
+  BACKGROUND: '#edebeb',
+  NAVBAR: '#f5f5f5',
+  FOOTER: '#f5f5f5',
+  BLACK_LABEL_TEXT: '#333333',
+  BLACK_INFO_TEXT: '#828282',
+  WHITE_LABEL_TEXT: '#f0eded',
+  CARD_BG: '#f5f5f5',
+  BLACK_CARD_BG: '#333333'
+}
+
